77808b40820382aa60346d7bcf59b0e9b78b0bc6
SamuelMasters <samuelmastersdev@gmail.com>
2022-04-07 18:37:09 +0000

    Add code to allow for changing of stock variables

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
 from pprint import pprint
+import time
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -17,6 +18,7 @@ SHEET = GSPREAD_CLIENT.open('Stock Allocation Tool: Dummy Data')
 inventory_data = SHEET.worksheet('dummy data')
 
 data = inventory_data.get_all_values()
+overstock = 1
 
 
 def introduction():
@@ -37,7 +39,9 @@ def show_menu():
     """
     options = ["1) Instructions", "2) Import Data", "3) Export Replenishment",
         "4) Adjust Variables", "5) Query Data", "6) Exit"]
+    print(f"{'-' * 50}")
     print(f"Please select an operation:\n")
+    time.sleep(1.5)
     for option in options:
         print(f"{option}\n")
     selection = input(f"To select an option, type the corresponding number, and press Enter.\n")
@@ -48,6 +52,8 @@ def get_specific_value(cell):
     """
     Returns the value of a specific cell as specified by the user.
     """
+    print(f"Retrieving data...\n")
+    time.sleep(2)
     specific_value = inventory_data.acell(cell).value
     print(specific_value)
     return specific_value
@@ -57,6 +63,8 @@ def get_col_values(col):
     """
     Returns and prints all values from the specified column.
     """
+    print(f"Retrieving data...\n")
+    time.sleep(2)
     col_values = inventory_data.col_values(col)
     pprint(col_values)
     return col_values
@@ -66,8 +74,24 @@ def get_all_data():
     """
     Returns and prints all values from the connected worksheet.
     """
+    print(f"Retrieving data...\n")
+    time.sleep(2)
     all_data = inventory_data.get_all_values()
     return all_data
+
+
+def adjust_variables():
+    """
+    Changes global variables according to user input.
+    """
+    global overstock
+    print(f"The current overstock multiplier is set to {overstock}.\n")
+    time.sleep(2)
+    overstock = float(input(f"Please type a multiplier in to represent desired overstock.\n"))
+    print(f"Adjusting variable...\n")
+    time.sleep(2)
+    print(f"The overstock multiplier is now set to {overstock}.\n")
+    time.sleep(2)
 
 
 def quit_program():
@@ -85,7 +109,7 @@ def handle_input(x):
     elif x == 3:
         pass
     elif x == 4:
-        pass
+        adjust_variables()
     elif x == 5:
         all_data = get_all_data()
         pprint(all_data)
@@ -97,8 +121,10 @@ def handle_input(x):
 
 def main():
     introduction()
-    x = int(show_menu())
-    handle_input(x)
+    time.sleep(2)
 
+    while True:
+        x = int(show_menu())
+        handle_input(x)
 
 main()
